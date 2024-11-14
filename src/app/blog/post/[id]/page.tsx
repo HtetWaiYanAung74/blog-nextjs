d7a2edb90b8e34f964c0b24f1e4849a3a9d3fa44
HtetWaiYanAung74 <htetwyag75@gmail.com>
2024-11-14 23:12:35 +0700

    loading and not found

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,11 +1,18 @@
-import { posts } from '@/app/lib/placeholder-data';
+import { getPosts } from '@/app/lib/data';
 import Post from '@/app/ui/components/posts/Post';
+import { notFound } from 'next/navigation';
 
 // export default async function Page({ params }: { params: { id: string } }) {
 export default async function PostPage({ params }: { params: Promise<{ id: string }> }) {
+  
+  const posts = await getPosts();
 
   const post_id: string = (await params).id;
-  const post = posts.find(post => post.id === post_id)!;
+  const post = posts?.find(post => post.id === '')!;
+
+  if (!post) {
+    notFound();
+  }
 
   return (
     <>
