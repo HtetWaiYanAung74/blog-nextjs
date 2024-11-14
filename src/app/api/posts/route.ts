441e7a955e44393cfe0a85a68f7ac9a30cb9c18e
HtetWaiYanAung74 <htetwyag75@gmail.com>
2024-11-14 22:40:18 +0700

    create new blog post

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,26 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const posts = await sql`SELECT * FROM posts ORDER BY date DESC LIMIT 7;`;
+    return NextResponse.json({ posts }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+export async function POST(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
+  const title = searchParams.get('title');
+  const content = searchParams.get('content');
+  const date = searchParams.get('date');
+
+  try {
+    const post = await sql`INSERT INTO posts (id, author, title, content, date) VALUES (${id}, 'Sandra L', ${title}, ${content}, ${date})`;
+    return NextResponse.json({ message: 'Post has been created successfully'}, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({error}, { status: 500 });
+  }
+}
--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import { connectToDB, getPosts } from '@/app/lib/data';
+import { Button } from '@/app/ui/components/button';
 import Post from '@/app/ui/components/posts/Post';
 
 export default async function Page() {
@@ -6,7 +8,12 @@ export default async function Page() {
   const posts = await getPosts();
   return (
     <>
-      { client && <p className='text-green-500'>Connected to database</p> }
+      { client && <p className='text-green-500 my-2'>Connected to database</p> }
+      <Link href='/blog/post/insert'>
+        <Button className='outline outline-1 border-emerald-700 text-emerald-500 hover:bg-emerald-700 hover:text-white my-5 py-2 px-4 rounded'>
+          New +
+        </Button>
+      </Link>
       <h1>Posts</h1>
       { posts?.map(post => <Post key={post.id} {...post} />)}
     </>
